451daa7734d649c9454d606e0fd416e213a1644d
Jonathan Blaney <Jonathan.Blaney@sas.ac.uk>
2016-08-19 12:05:27 +0100

    modified python script to wrap all html in one big list; added source and output files to repo

--- generate/htmlify-vocabulary.py
+++ generate/htmlify-vocabulary.py
@@ -7,16 +7,16 @@ with open ('bbih-vocabulary-as-xml.xml', 'r') as f1:
 
 text = re.sub('\n', '', text)
 text = re.sub('<lev', '\n<lev', text)
-text = re.sub('<bbih-vocab>', '\n<html><head><title>Bibliography of British and Irish History vocabulary explorer</title></head><body>', text)
-text = re.sub('</bbih-vocab>', '\n</body></html>', text)
+text = re.sub('<bbih-vocab>', '\n<html><head><title>Bibliography of British and Irish History vocabulary explorer</title></head><body><ul><li>Bibliography of British and Irish History vocabulary explorer', text)
+text = re.sub('</bbih-vocab>', '\n</li></ul></body></html>', text)
 text = re.sub('<lev[0-9]+><rhs[^>]+>', '<ul><li>', text)
 text = re.sub('</rhs>\n', '\n', text)
 text = re.sub('</lev[0-9]+>', '</li></ul>', text)#is this line right?
-text = re.sub('<usedFor><rhs id="[0-9]+" multi="yes">', '<span style="usedFor-multi">', text)
-text = re.sub('<usedFor><rhs id="[0-9]+" multi="yes" factor="yes">', '<span style="usedFor-multi-factor">', text)
-text = re.sub('<usedFor><rhs id="[0-9]+">', '<span style="usedFor">', text)
+text = re.sub('<usedFor><rhs id="[0-9]+" multi="yes">', '<span class="usedFor-multi">', text)
+text = re.sub('<usedFor><rhs id="[0-9]+" multi="yes" factor="yes">', '<span class="usedFor-multi-factor">', text)
+text = re.sub('<usedFor><rhs id="[0-9]+">', '<span class="usedFor">', text)
 text = re.sub('</rhs></usedFor>', '</span>', text)
-text = re.sub('<relatedTerm><rhs id="[0-9]+">', '<span style="relatedTerm">', text)
+text = re.sub('<relatedTerm><rhs id="[0-9]+">', '<span class="relatedTerm">', text)
 text = re.sub('</rhs></relatedTerm>', '</span>', text)
 text = re.sub('</rhs></li>', '</li>', text)
 
