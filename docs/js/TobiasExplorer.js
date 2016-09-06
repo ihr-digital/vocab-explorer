1cc00d92dcbcd26165e3dfa730dda86a76f4a5b7
Marty steer <martin.steer@sas.ac.uk>
2016-09-06 10:59:21 +0100

    refactor tobias tangle to decouple from other js on the page.

--- docs/js/TobiasExplorer.js
+++ docs/js/TobiasExplorer.js
@@ -33,32 +33,5 @@ window.addEvent('domready', function(){
     });
 
 
-    new Tree('treeView');
-    setUpTangle();
+    // new Tree('treeView');
 });
-
-function setUpTangle () {
-	var element = document.getElementById("skosConceptMain");
-
-	var tangle = new Tangle(element, {
-		initialize: function () {
-			this.concept = 'Religion and faith';
-			this.uriFragment = 'http://www.tobias.org';
-			this.ident = 100;
-		},
-		update: function () {
-			this.conceptDisplay = this.concept;
-			this.uriFragmentDisplay = this.uriFragment;
-			this.uriFragmentForURIDisplay = cleanText(this.concept);
-			this.identDisplay = this.ident;
-		}
-	});
-}
-
-/* Function to transform plain text for the URI.
- * Removes punctuation, lowercase characters, transform space to underscore.
- */
-function cleanText(txt) {
-	newTxt = txt.replace(/ /g, '_').replace(/[.,\/#!$%\^&\*;:{}=\-'`~()]/g,"").toLowerCase();
-	return newTxt;
-}
--- docs/js/TobiasTangle.js
+++ docs/js/TobiasTangle.js
@@ -1,8 +1,6 @@
 //
 // Tobias Tangle classes
 //
-
-
 (function () {
 
 //----------------------------------------------------------
@@ -22,28 +20,61 @@ Tangle.classes.TBTextField = {
     		"class":"TBTextFieldInput",
     		size: options.size || 6
         }).inject(element, "top");
-        
+
         var inputChanged = (function () {
             var value = this.getValue();
             tangle.setValue(variable, value);
         }).bind(this);
-        
+
         this.input.addEvent("keyup",  inputChanged);
         this.input.addEvent("blur",   inputChanged);
         this.input.addEvent("change", inputChanged);
 	},
-	
+
 	getValue: function () {
         var value = this.input.get("value");  // TODO: sanitise string value?
         return value;
 	},
-	
+
 	update: function (element, value) {
 	    var currentValue = this.getValue();
 	    if (value !== currentValue) { this.input.set("value", "" + value); }
 	}
 };
-    
 //----------------------------------------------------------
 
 })();
+
+
+// Tangle Setup and other functions.
+function setUpTangle () {
+    var element = document.getElementById("skosConceptMain");
+
+    var tangle = new Tangle(element, {
+        initialize: function () {
+            this.concept = 'Religion and faith';
+            this.uriFragment = 'http://www.tobias.org';
+            this.ident = 100;
+        },
+        update: function () {
+            this.conceptDisplay = this.concept;
+            this.uriFragmentDisplay = this.uriFragment;
+            this.uriFragmentForURIDisplay = cleanText(this.concept);
+            this.identDisplay = this.ident;
+        }
+    });
+}
+
+
+// Function to transform plain text for the URI.
+// Removes punctuation, lowercase characters, transform space to underscore.
+function cleanText(txt) {
+    newTxt = txt.replace(/ /g, '_').replace(/[.,\/#!$%\^&\*;:{}=\-'`~()]/g,"").toLowerCase();
+    return newTxt;
+}
+
+
+// Initialise Tobias Tangle
+window.addEvent('domready', function(){
+    setUpTangle();
+});
