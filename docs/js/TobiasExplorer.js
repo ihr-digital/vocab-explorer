12634e298888e4da23c67b42f0cae7c1dbaced03
Marty steer <martin.steer@sas.ac.uk>
2016-09-06 10:26:51 +0100

    small changes for accesible tree

--- docs/js/TobiasExplorer.js
+++ docs/js/TobiasExplorer.js
@@ -3,6 +3,36 @@
  * TOBIAS Project, IHR Digital, 2016-07
  */
 window.addEvent('domready', function(){
+    // Extend the Mootools Element with show/hide/toggle functions.
+	Element.implement({
+		//implement show
+		show: function() {
+			this.setStyle('display','');
+		},
+		//implement hide
+		hide: function() {
+			this.setStyle('display','none');
+		},
+		// implement toggle
+		toggle: function() {
+			console.log('toggle');
+			if (this.getStyle('display') != 'none') {
+				this.hide();
+			} else {
+				this.show();
+			}
+		}
+	});
+
+    var synSelector = '.usedFor, .relatedTerm, .usedFor-multi';
+    $$(synSelector).hide();
+
+    // Setup toggle click function
+    $$('#toggleSyns').addEvent('click', function(e) {
+        $$(synSelector).toggle();
+    });
+
+
     new Tree('treeView');
     setUpTangle();
 });
