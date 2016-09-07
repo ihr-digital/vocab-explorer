ed9c054a932766c1e677c8b7e4149199e893c0f7
Marty steer <martin.steer@sas.ac.uk>
2016-09-07 08:50:12 +0100

    implemented jstree

--- docs/js/TobiasExplorer.js
+++ docs/js/TobiasExplorer.js
@@ -3,35 +3,15 @@
  * TOBIAS Project, IHR Digital, 2016-07
  */
 window.addEvent('domready', function(){
-    // Extend the Mootools Element with show/hide/toggle functions.
-	Element.implement({
-		//implement show
-		show: function() {
-			this.setStyle('display','');
-		},
-		//implement hide
-		hide: function() {
-			this.setStyle('display','none');
-		},
-		// implement toggle
-		toggle: function() {
-			console.log('toggle');
-			if (this.getStyle('display') != 'none') {
-				this.hide();
-			} else {
-				this.show();
-			}
-		}
-	});
-
     var synSelector = '.usedFor, .relatedTerm, .usedFor-multi';
-    $$(synSelector).hide();
+    $(synSelector).hide();
 
     // Setup toggle click function
-    $$('#toggleSyns').addEvent('click', function(e) {
-        $$(synSelector).toggle();
+    $('#toggleSyns').click(function() {
+        $(synSelector).toggle();
     });
 
 
-    // new Tree('treeView');
+    $('#jsTree1').jstree();
+    $('#jsTree2').jstree();
 });
