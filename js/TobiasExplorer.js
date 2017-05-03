2e89dc79e336bbfef497c08041b0a1a1e1a15896
Marty steer <martin.steer@sas.ac.uk>
2017-05-03 14:50:10 +0100

    started working on init and select handlers

--- js/TobiasExplorer.js
+++ js/TobiasExplorer.js
@@ -2,22 +2,32 @@
  * BBIH Vocabulary as a HTML tree view
  * TOBIAS Project, IHR Digital, 2017-05
  */
+
+// The synonymn span selector and state variable for visibility
+var synSelector = '.usedFor, .relatedTerm, .usedFor-multi';
+var synVisible = false;
+
 window.addEvent('domready', function(){
-    var synSelector = '.usedFor, .relatedTerm, .usedFor-multi';
-    $(synSelector).hide();
-
-    // Setup toggle function for button click and keypress
-    $('#toggleSyns').click(function() {
-        $(synSelector).toggle();
-    });
-    $(document).keypress(function(event) {
-      if (event.keyCode == 116)  // Key press 't'
-        $(synSelector).toggle();
-    });
-
-    // Initialise the tree
+    // Initialise the tree and setup handlers
     // $.jstree.defaults.core.themes.variant = "large";
-    $('#tobias-jsTree').jstree({
+    $('#tobias-jsTree')
+    // Listen for events
+    // As nodes are opened, obey the synonymn visibility state
+    .on('before_open.jstree', function (e, data) {
+      // console.log(data);
+      if (synVisible == false) {
+        $(this).find(synSelector).hide();
+      } else {
+        $(this).find(synSelector).show();
+      }
+    })
+    // When nodes are selected, populate the SKOSConceptMain code box
+    .on('select_node.jstree', function (e, data) {
+      console.log('select node', $(data.selected));
+      // console.log(data);
+    })
+    // Create/init the tree
+    .jstree({
       'core': {
         'data' : {
           'url' : 'data/bbih-vocabulary-sample.html',  // Get the tree list from here.
@@ -33,7 +43,29 @@ window.addEvent('domready', function(){
       },
       // Customise some types of nodes for icons
       // Keep the opened/selected state of the tree
-      // Select the whole row
-      'plugins' : [ 'types', 'state', 'wholerow' ],
+      'plugins' : [ 'types', 'state' ],
     });
+
+    // Keep the SKOS markup box on screen as you scroll down.
+    var $window = $(window),
+        $sticky = $('#skosConceptMain'),
+        stickyTop = $sticky.offset().top;
+    $window.scroll(function() {
+      $sticky.toggleClass('sticky', $window.scrollTop() > stickyTop);
+    });
+
+    // Setup toggle synonymns function for button click and keypress
+    $('#toggleSyns').click(function() {
+      console.log(synVisible);
+      $(synSelector).toggle();
+      synVisible = !synVisible;
+    });
+    $(document).keypress(function(event) {
+      if (event.keyCode == 116)  { // Key press 't'
+        console.log(synVisible);
+        $(synSelector).toggle();
+        synVisible = !synVisible;
+      }
+    });
+
 });
